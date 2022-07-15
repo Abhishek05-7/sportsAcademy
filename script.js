bd53ae988c1223678487d5313192f0e5b8036d12
Abhishek Singh <79144643+Abhishek05-7@users.noreply.github.com>
2022-07-15 16:12:59 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,86 @@
+<!-- Facebook Pixel Code -->
+<script>
+  !function(f,b,e,v,n,t,s)
+  {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+  n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+  if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+  n.queue=[];t=b.createElement(e);t.async=!0;
+  t.src=v;s=b.getElementsByTagName(e)[0];
+  s.parentNode.insertBefore(t,s)}(window, document,'script',
+  'https://connect.facebook.net/en_US/fbevents.js');
+  fbq('init', '225743612333493');
+  fbq('track', 'PageView');
+
+</script>
+
+<noscript><img height="1" width="1" style="display:none"
+  src="https://www.facebook.com/tr?id=225743612333493&ev=PageView&noscript=1"
+/></noscript>
+<!-- End Facebook Pixel Code -->
+<!-- Global site tag (gtag.js) - Google Ads: 649958479 -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=AW-649958479"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'AW-649958479');
+</script>
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-186959858-1"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-186959858-1');
+</script>	
+
+
+<script type="application/ld+json" class="aioseo-schema">
+			{"@context":"https:\/\/schema.org","@graph":[{"@type":"WebSite","@id":"https:\/\/ramagyasportsacademy.com\/#website","url":"https:\/\/ramagyasportsacademy.com\/","name":"Ramagya Sports Academy","description":"Best Sports Academy In India","publisher":{"@id":"https:\/\/ramagyasportsacademy.com\/#organization"}},{"@type":"Organization","@id":"https:\/\/ramagyasportsacademy.com\/#organization","name":"Ramagya Sports Academy","url":"https:\/\/ramagyasportsacademy.com\/"},{"@type":"BreadcrumbList","@id":"https:\/\/ramagyasportsacademy.com\/#breadcrumblist","itemListElement":[{"@type":"ListItem","@id":"https:\/\/ramagyasportsacademy.com\/#listItem","position":"1","item":{"@id":"https:\/\/ramagyasportsacademy.com\/#item","name":"Home","description":"Best Sports Academy In India","url":"https:\/\/ramagyasportsacademy.com\/"}}]},{"@type":"WebPage","@id":"https:\/\/ramagyasportsacademy.com\/#webpage","url":"https:\/\/ramagyasportsacademy.com\/","name":"Ramagya Sports Academy","inLanguage":"en-US","isPartOf":{"@id":"https:\/\/ramagyasportsacademy.com\/#website"},"breadcrumb":{"@id":"https:\/\/ramagyasportsacademy.com\/#breadcrumblist"},"datePublished":"2017-09-05T06:00:28+00:00","dateModified":"2022-07-07T08:22:56+00:00"}]}
+		</script>
+		<script type="text/javascript" >
+			window.ga=window.ga||function(){(ga.q=ga.q||[]).push(arguments)};ga.l=+new Date;
+			ga('create', "UA-117674854-1", 'auto');
+			ga('send', 'pageview');
+		</script>
+		<script async src="https://www.google-analytics.com/analytics.js"></script>
+		<!-- All in One SEO -->
+
+
+<script type="text/javascript">
+			window._wpemojiSettings = {"baseUrl":"https:\/\/s.w.org\/images\/core\/emoji\/13.0.1\/72x72\/","ext":".png","svgUrl":"https:\/\/s.w.org\/images\/core\/emoji\/13.0.1\/svg\/","svgExt":".svg","source":{"concatemoji":"https:\/\/ramagyasportsacademy.com\/wp-includes\/js\/wp-emoji-release.min.js?ver=5.7.6"}};
+			!function(e,a,t){var n,r,o,i=a.createElement("canvas"),p=i.getContext&&i.getContext("2d");function s(e,t){var a=String.fromCharCode;p.clearRect(0,0,i.width,i.height),p.fillText(a.apply(this,e),0,0);e=i.toDataURL();return p.clearRect(0,0,i.width,i.height),p.fillText(a.apply(this,t),0,0),e===i.toDataURL()}function c(e){var t=a.createElement("script");t.src=e,t.defer=t.type="text/javascript",a.getElementsByTagName("head")[0].appendChild(t)}for(o=Array("flag","emoji"),t.supports={everything:!0,everythingExceptFlag:!0},r=0;r<o.length;r++)t.supports[o[r]]=function(e){if(!p||!p.fillText)return!1;switch(p.textBaseline="top",p.font="600 32px Arial",e){case"flag":return s([127987,65039,8205,9895,65039],[127987,65039,8203,9895,65039])?!1:!s([55356,56826,55356,56819],[55356,56826,8203,55356,56819])&&!s([55356,57332,56128,56423,56128,56418,56128,56421,56128,56430,56128,56423,56128,56447],[55356,57332,8203,56128,56423,8203,56128,56418,8203,56128,56421,8203,56128,56430,8203,56128,56423,8203,56128,56447]);case"emoji":return!s([55357,56424,8205,55356,57212],[55357,56424,8203,55356,57212])}return!1}(o[r]),t.supports.everything=t.supports.everything&&t.supports[o[r]],"flag"!==o[r]&&(t.supports.everythingExceptFlag=t.supports.everythingExceptFlag&&t.supports[o[r]]);t.supports.everythingExceptFlag=t.supports.everythingExceptFlag&&!t.supports.flag,t.DOMReady=!1,t.readyCallback=function(){t.DOMReady=!0},t.supports.everything||(n=function(){t.readyCallback()},a.addEventListener?(a.addEventListener("DOMContentLoaded",n,!1),e.addEventListener("load",n,!1)):(e.attachEvent("onload",n),a.attachEvent("onreadystatechange",function(){"complete"===a.readyState&&t.readyCallback()})),(n=t.source||{}).concatemoji?c(n.concatemoji):n.wpemoji&&n.twemoji&&(c(n.twemoji),c(n.wpemoji)))}(window,document,window._wpemojiSettings);
+		</script>
+
+ <script type="text/javascript">
+				jQuery(document).ready(function($){
+               		$('.ult-carousel-133479685062d112522a7b9').slick({dots: false,autoplay: true,autoplaySpeed: 4000,speed: 500,infinite: true,arrows: true,nextArrow: '<button type="button" style="color:#333333; font-size:0px;" class="slick-next default"><i class="ultsl-arrow-right6"></i></button>',prevArrow: '<button type="button" style="color:#333333; font-size:0px;" class="slick-prev default"><i class="ultsl-arrow-left6"></i></button>',slidesToScroll:1,slidesToShow:1,swipe: true,draggable: true,touchMove: true,responsive: [
+							{
+							  breakpoint: 1025,
+							  settings: {
+								slidesToShow: 1,
+								slidesToScroll: 1,  
+							  }
+							},
+							{
+							  breakpoint: 769,
+							  settings: {
+								slidesToShow: 1,
+								slidesToScroll: 1
+							  }
+							},
+							{
+							  breakpoint: 481,
+							  settings: {
+								slidesToShow: 1,
+								slidesToScroll: 1
+							  }
+							}
+						],pauseOnHover: true,
+						pauseOnDotsHover: true,customPaging: function(slider, i) {
+                   return '<i type="button" style="color:#333333;" class="ultsl-record" data-role="none"></i>';
+                },});
+				});
+			</script>
